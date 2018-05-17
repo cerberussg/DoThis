919491af5e3c07d7264ce458b7aaa8af727974e0
Scott Goyette <sgoyette@kerberosfoundation.com>
2018-05-17 00:13:05 -0400

    Added gritter for notifications

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery3
 //= require popper
 //= require bootstrap
+//= require gritter
 //= require activestorage
 //= require turbolinks
 //= require_tree .
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :user
 
+  validates_presence_of :title, :description
+
   def completed?
     !completed_at.blank?
   end
