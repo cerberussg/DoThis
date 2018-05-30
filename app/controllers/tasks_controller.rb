6f6330368c43b8a40755c3331810b541d9a079f1
Scott Goyette <sgoyette@kerberosfoundation.com>
2018-05-30 00:19:22 -0400

    fixed feature

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -57,7 +57,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :description)
+    params.require(:task).permit(:title, :description, :status)
   end
 
   def find_task
